af682a19478cd79732a2109d481aa2ff6c249fa4
Yuxiang Yang <yuxiang.yang@quantropi.com>
2024-04-09 15:36:52 -0400

    Components

--- backend/src/routes/reposRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const repoController = require('../controllers/reposController');
-
-router.get('/repos', repoController.getRepositories);
-
-module.exports = router;
